48d64f29e1321ac31c972e2cd2793c7fb7e1734f
jeeskala-netizen <jeeskala@gmail.com>
2025-11-26 01:51:51 +0100

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,9 @@ try:
 except:
     # هذه المفاتيح احتياطية للعمل على جهازك فقط (Localhost)
     # لكن عند الرفع، السيرفر سيستخدم الـ secrets
-    TMDB_API_KEY = "92a2bfcd6e2b85e41ca046e16c94ac03"
-    GROQ_API_KEY = "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"
-
+   
 # روابط النظام
 BASE_URL = "https://api.themoviedb.org/3"
 IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
 BACKDROP_URL = "https://image.tmdb.org/t/p/original"
